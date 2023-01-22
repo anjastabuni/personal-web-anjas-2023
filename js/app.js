4f05b8181505e83b01f4a3a4596dceaf15891be0
username_anjastabuni <reblytabuni2004@gmail.com>
2023-01-22 18:10:53 +0700

    ubah color

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function redirInstagram() {
+  window.location.href = "https://instagram.com/mekal.tabuni";
+}
+{
+  /* function redirWhatsapp() {
+            window.location.href = "https://wa.me/+6281343109866"
+        } */
+}
